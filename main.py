8a6fa0a2ee215a0e6cddd9453ab30881fba41b66
PochtiPlusovik <potehamihail@gmail.com>
2026-08-31 21:46:36 +1000

    Исправлен KeyboardInterrupt, добавлен os._exit(0)

--- main.py
+++ main.py
@@ -9,47 +9,51 @@ def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-
 engine = chess.engine.SimpleEngine.popen_uci("/home/pion/Stockfish/src/stockfish")
 
 try:
 
     win = 0
     while win < 1:
-        print(board)
-        hod = input("Ваш ход: ")
-
-        if hod == "exit":
-            print("Вы вышли.")
-            exit()
-
-        move = chess.Move.from_uci(hod)
-        board.push(move)
-        clear_console()
-        print(board)
-
-        usr = 2
-        while usr > 1:
-            result = engine.play(board, chess.engine.Limit(time=2))
+        try:
             clear_console()
-            board.push(result.move)
             print(board)
-            break
+            hod = input("Ваш ход: ")
 
-        if board.is_checkmate():
-            print("Мат!")
-            win += 2
-        elif board.is_stalemate():
-            print("Пат!")
-            win += 2
-        elif board.is_check():
+            if hod == "exit":
+                print("Вы вышли. Игра окончена.")
+                os._exit(0)
+
+            move = chess.Move.from_uci(hod)
+            board.push(move)
+            clear_console()
+            print(board)
+
+            usr = 2
+            while usr > 1:
+                result = engine.play(board, chess.engine.Limit(time=2))
+                clear_console()
+                board.push(result.move)
+                print(board)
+                break
+
+            if board.is_checkmate():
+                print("Мат!")
+                win += 2
+            elif board.is_stalemate():
+                print("Пат!")
+                win += 2
+            elif board.is_check():
+                print("Невозможный код!")
+                win += 2
+
+        except chess.InvalidMoveError:
             print("Невозможный ход!")
-            win += 2
 
         continue
 
 except KeyboardInterrupt:
-    print("\nПрограмма прервана пользователем")
+    print("\nПрограмма прервана пользователем.")
 
 print("Игра окончена.")
-exit()
+os._exit(0)
